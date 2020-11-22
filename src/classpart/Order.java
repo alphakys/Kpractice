4d9e9142549866b970538aff3c44eace0d63401b
gys11 <gys11@DESKTOP-SOQODPI.kornet>
2020-11-22 20:00:20 +0900

    11.22

--- src/classpart/Order.java
+++ src/classpart/Order.java
@@ -2,4 +2,30 @@ package classpart;
 
 public class Order {
 
+	
+	
+		int orderNum;
+		String ID;
+		String orederDate;
+		String name;
+		String productNum;
+		String address;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
---
+++ src/hiding/StudentTest.java
@@ -0,0 +1,18 @@
+package hiding;
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+					
+		Student st = new Student();
+		
+		st.setStudentName("강용수");
+		
+		System.out.println(st.getName());
+		
+		
+		
+	}
+
+}
