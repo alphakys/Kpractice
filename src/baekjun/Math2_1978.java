e3059129057ff47215959307a0388a222b8f7023
'alphakys' <'gys1120@naver.com'>
2020-11-24 18:55:30 +0900

    11.24

--- src/baekjun/Math2_1978.java
+++ src/baekjun/Math2_1978.java
@@ -1,10 +1,52 @@
 package baekjun;
 
+import java.util.Scanner;
+
 public class Math2_1978 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+					
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		int[] K = new int[N];
+		
+		
+		
+		for(int i=0;i<K.length;i++) {
+			K[i] = sc.nextInt();
+			
+		}
+		
+		int count=0;
+		int countA=0;
+		
+		
+		for(int j=2;j<=K[0];j++ ) {
+			
+			if(K[0]%j==0) {
+						count++;
+							if(count>2) {
+								break;
+								
+							 }
+							else {
+								countA++;
+							 }
+						  }
+			
+			
+			
+			
+								 }
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
